74d13b051b5306e5c42b86908e4621d727aa7bdc
hmndzzz <hrmendezlee@gmail.com>
2024-04-23 20:20:14 -0600

    git commit -m "prueba"

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 import tkinter as tk
 
+import tkinter as tk
+
+def saludar():
+    label_saludo.config(text="¡Hola, Mundo!")
+
+root = tk.Tk()
+root.title("Mi Aplicación")
+root.geometry("400x300")
+
+# Etiqueta
+label_saludo = tk.Label(root, text="¡Bienvenido!")
+label_saludo.pack(pady=20)  # pady es el espacio vertical alrededor del widget
+
+# Botón
+boton_saludar = tk.Button(root, text="Saludar", command=saludar)
+boton_saludar.pack()
+
+root.mainloop()
