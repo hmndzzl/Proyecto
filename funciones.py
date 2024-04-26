79f2a27093e25d796bf667fd95c65059d5b01760
hmndzzz <hrmendezlee@gmail.com>
2024-04-25 23:15:42 -0600

    commit -m "cambiar funcion crear_usuario y agregar funcion obtener_datos"

--- funciones.py
+++ funciones.py
@@ -1,15 +1,23 @@
+import datetime as dt
+
 def crear_usario(usuarios):
-    usuario = input("Ingrese nombre de usuario: ")
-    contraseña_valida = False
-    while contraseña_valida == False: 
-        contraseña = input("Ingrese contraseña: ")
-        contraseña2 = input("Ingrese de nuevo su contraseña: ")
-        if contraseña == contraseña2:
-            contraseña_valida = True
-        else:
-            print("Las contraseñas no coinciden, intente de nuevo")
-    usuarios[usuario] = contraseña
-    print(f"Usuario {usuario} creado exitosamente")
+    usuario_valido = False
+    while usuario_valido == False:
+        usuario = input("Ingrese nombre de usuario: ")
+        if usuario in usuarios.keys():
+            print("Error el usuario ya está tomado.")
+        else: 
+            contraseña_valida = False
+            while contraseña_valida == False: 
+                contraseña = input("Ingrese contraseña: ")
+                contraseña2 = input("Ingrese de nuevo su contraseña: ")
+                if contraseña == contraseña2:
+                    contraseña_valida = True
+                else:
+                    print("Las contraseñas no coinciden, intente de nuevo")
+            usuarios[usuario] = contraseña
+            print(f"Usuario {usuario} creado exitosamente")
+            usuario_valido = True
 
 def ingresar(usuarios):
     usuario = input("Ingrese nombre de usuario: ")
@@ -22,3 +30,13 @@ def ingresar(usuarios):
     else:
         print("El usuario no existe")
 
+def obtener_datos(datos):
+    index = 0
+    dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
+    for i in range(0,7):
+        ph = datos["pH"][index]
+        temperatura = datos["Temperatura"][index]
+        humedad = datos["Humedad"][index]
+        print(f"Datos día {dias[index]}: ph: {ph}, Temperatura: {temperatura}, Humedad: {humedad}%")
+        index += 1
+        
