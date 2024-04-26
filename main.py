d98c6d290b5a6a987766453d62197fecb76bb1cb
Diego-glitch-cloud <123759607+Diego-glitch-cloud@users.noreply.github.com>
2024-04-25 23:17:05 -0600

    menú  1

--- main.py
+++ main.py
@@ -13,17 +13,41 @@ menu1 = """
 1 = Iniciar Sesión
 2 = Crear Cuenta"""
 
+menu2 = """
+1 = Obtener Base de Datos Semanal
+2 = """
+
+def menu(menu):
+    # Imprime menú  
+    print(menu)  
+    opcion = int(input("Ingrese el número de la opción que desea: "))
+    # Si el usuario ingresa un valor fuera del rango aceptado:
+    while opcion not in list(range(1, 3)):
+        print("Ha ingresado un valor fuera de los aceptados, vuelva a intentarlo:")
+        print(menu)  
+        opcion = int(input("Ingrese el número de la opción que desea: "))
+    return opcion
+
+
 # Menú 1: Inicio de Sesión
 w = True
 while w:
-    # Imprime menú 1 
-    print(menu1)  
-    opcion = input("Ingrese el número de la opción que desea: ")
-    # Si el usuario ingresa un valor fuera del rango aceptado:
-    while opcion not in range(1, 3):
-        print("Ha ingresado un valor fuera de los aceptados, vuelva a intentarlo: ")
-        print(menu1)  
-        opcion = input("Ingrese el número de la opción que desea: ")
+    
+    menu(menu1)
+    
+    if opcion == 1:
+        print("función de iniciar sesión")
+        
+    if opcion == 2:
+        print("función de crear una cuenta")
+        print("función para iniciar sesión")
+    
+    # Segundo Menú: 
+    t = True    
+    while t:
+        
+        
+        
         
     
     
