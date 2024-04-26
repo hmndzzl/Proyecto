037bcfe7436a0670fe4d9cc12ca732759d71851d
Diego-glitch-cloud <123759607+Diego-glitch-cloud@users.noreply.github.com>
2024-04-25 22:07:58 -0600

    Primer Menú

--- main.py
+++ main.py
@@ -8,3 +8,22 @@
 # Arodi Josué Chávez Ramírez - 241112
 #---------------------------------
 
+# Variables
+menu1 = """
+1 = Iniciar Sesión
+2 = Crear Cuenta"""
+
+# Menú 1: Inicio de Sesión
+w = True
+while w:
+    # Imprime menú 1 
+    print(menu1)  
+    opcion = input("Ingrese el número de la opción que desea: ")
+    # Si el usuario ingresa un valor fuera del rango aceptado:
+    while opcion not in range(1, 3):
+        print("Ha ingresado un valor fuera de los aceptados, vuelva a intentarlo: ")
+        print(menu1)  
+        opcion = input("Ingrese el número de la opción que desea: ")
+        
+    
+    
